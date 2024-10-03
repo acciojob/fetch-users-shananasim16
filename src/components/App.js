8c815b602ad2f8681c7c5908e119ec30560b3941
shananasim16 <shanashabnam02@gmail.com>
2024-10-03 19:57:15 +0530

    fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,66 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  function btnClickHandler() {
+    setLoading(true); // Start loading
+    fetch("https://reqres.in/api/users")
+      .then(rawData => rawData.json())
+      .then(json => {
+        setData(json.data);
+        setLoading(false); // Stop loading
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false); // Stop loading in case of error
+      });
+  }
+
+  if(!data.length)
+  {
+    return (
+      <div>
+
+      <button className="btn" onClick={btnClickHandler}>
+        {loading ? "Loading..." : "Get User List"}
+      </button>
+
+      <p>No data found to display.</p>
+
+      <table style={{height:"100px", width:"300px"}}>
+
+      </table>
+
+      </div>
+
+    )
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button className="btn" onClick={btnClickHandler}>
+        {loading ? "Loading..." : "Get User List"}
+      </button>
+
+      <table>
+        <tbody>
+       
+            {data.map((item, index) => (
+              <tr key={index}>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+                <td><img src={item.avatar} alt="User Avatar" /></td>
+              </tr>
+            ))}
+          
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
